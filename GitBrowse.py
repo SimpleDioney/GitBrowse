d2f2cb465b245f0b6254b3f634795ca1a5c10e7e
SimpleDioney <62361235+SimpleDioney@users.noreply.github.com>
2024-05-04 22:42:21 -0300

    Add files via upload

--- GitBrowse.py
+++ GitBrowse.py
@@ -15,7 +15,7 @@ ERROR = F.RED
 SUCCESS = F.GREEN
 INFO = F.CYAN
 WHITE = F.WHITE
-HEADER= F.LIGHTMAGENTA_EX
+HEADER = F.LIGHTMAGENTA_EX
 FOOTER = F.MAGENTA
 
 lock = threading.Lock()  # Mutex para sincronização
@@ -52,7 +52,6 @@ def get_default_branch(username, repo_name):
         repo_data = response.json()
         return repo_data.get('default_branch', 'main')
     else:
-        print(f"{PREFIX_OUT}Erro ao buscar informações do repositório: Status code {ERROR}{response.status_code}{RESET}")
         return 'main'
 
 def fetch_repository_files(username, repo, files_dict):
@@ -122,27 +121,42 @@ def list_repository_files(repo_url, default_branch='main'):
 def clone_repository(repo_url, repo_name):
     subprocess.run(["git", "clone", f"https://github.com{repo_url}.git", f"./{repo_name}"])
     clear_screen()
-    print(f"\n{PREFIX_OUT}Repositório {WHITE}'{repo_name}'{HEADER} clonado com sucesso.{RESET}")
+    print(f"\n{PREFIX_OUT} Repositório {WHITE}'{repo_name}'{HEADER} clonado com sucesso.{RESET}")
+
+def print_centered_header(text, total_width=30):
+    text_length = len(text)
+    if text_length < total_width:
+        padding = (total_width - text_length) // 2
+        return '-' * padding + text + '-' * (total_width - text_length - padding)
+    else:
+        return text
 
 def main():
     clear = lambda: os.system(get_clear_command())
     clear()
 
-    print(f"{HEADER}==================== BEM-VINDO AO GitBrowser ===================={RESET}")
-    print(f"{PREFIX_OUT}Para começar, digite o nome do usuário do GitHub:")
+    print(f"{HEADER}==================== BEM-VINDO AO GitBrowse ===================={RESET}")
+    print(f"{PREFIX_OUT} Para começar, digite o nome do usuário do GitHub:")
     username = input(f"{PREFIX_IN}{WHITE}")
 
-    print(f"{PREFIX_OUT}Procurando o usuário {WHITE}{username}{HEADER}...{RESET}")
+    print(f"{PREFIX_OUT} Procurando o usuário {WHITE}{username}{HEADER}...{RESET}")
 
     repositories = []
     files_dict = {}
     if not fetch_repositories(username, repositories, files_dict):
-        print(f"{PREFIX_OUT}{F.RED}Usuário não encontrado.{RESET}")
+        print(f"{PREFIX_OUT} {F.RED}Usuário não encontrado.{RESET}")
         return
     
     clear_screen()
-    print(f"{PREFIX_OUT}Usuário {WHITE}{username}{HEADER} encontrado! Navegue pelas opções abaixo:{RESET}")
-    print(RESET)
+    print(f"{PREFIX_OUT} Usuário {WHITE}{username}{HEADER} encontrado! Navegue pelas opções abaixo:{RESET}")
+    
+    current_page = 0
+    repos_per_page = 10
+    total_pages = (len(repositories) + repos_per_page - 1) // repos_per_page
+    header_width = max(30, len(username) + 10)
+
+
+
     while True:
         print(f"\n{WHITE}------------------------{RESET}\n")
         print(f"{WHITE}Opções:\n\n{WHITE}1.{HEADER} Listar repositórios\n\n{WHITE}2.{HEADER} Sair{RESET}")
@@ -150,41 +164,52 @@ def main():
         option = input(f"{PREFIX_OUT} {WHITE}Escolha uma opção:{WHITE} {RESET}")
 
         if option == '1':
-            clear_screen()
-            print(f"\n{WHITE}------------------------{RESET}\n")
-            for i, repo in enumerate(repositories, 1):
-                print(f"\n{i}. {HEADER}{repo['name']}{S.RESET_ALL}\n")
-            print(f"\n{WHITE}------------------------{RESET}\n")
-            repo_option = input(f"{PREFIX_OUT} \nSelecione um número para ver detalhes ou {ERROR}'b'{HEADER} para voltar: {WHITE}{RESET}")
-            if repo_option.isdigit():
+            while True:
                 clear_screen()
-                repo_index = int(repo_option) - 1
-                repo_name = repositories[repo_index]['name']
-                print(f"\n{WHITE}------------------------{RESET}\n")
-                print(f"\nOpções para {repo_name}:\n\n{WHITE}1.{HEADER} Ver arquivos\n\n{WHITE}2.{HEADER} Clonar repositório{RESET}")
-                print(f"\n{WHITE}------------------------{RESET}\n")
-                action = input(f"{PREFIX_OUT} {WHITE}Escolha uma ação: {WHITE}{RESET} ")
-                if action == '1':
+                start = current_page * repos_per_page
+                end = min(start + repos_per_page, len(repositories))
+                print(f"\n{WHITE}{print_centered_header(f'[{username}]', header_width)}{RESET}\n")
+                start = current_page * repos_per_page
+                end = min(start + repos_per_page, len(repositories))
+                for i in range(start, end):
+                    print(f"{i + 1}. {HEADER}{repositories[i]['name']}{RESET}")
+                print(f"\n{WHITE}{print_centered_header(f'[Página {current_page + 1}/{total_pages}]', header_width)}{RESET}\n")
+                print(f"{WHITE}Selecione um {HEADER}número {WHITE}para ver detalhes\nSelecione {HEADER}'p' {WHITE}para próxima página\nSelecione {HEADER}'n' {WHITE}para página anterior\nSelecione {HEADER}'b' {WHITE}para voltar ao menu anterior{WHITE}{RESET}")
+                repo_option = input(f"{PREFIX_OUT} {WHITE}Escolha uma ação: {WHITE}{RESET} ")
+                
+                if repo_option.isdigit():
+                    repo_index = int(repo_option) - 1 + start
+                    repo_name = repositories[repo_index]['name']
                     clear_screen()
-                    if repo_name in files_dict:
-                        files = files_dict[repo_name]
-                        for i, (display_name, file_url, file_type) in enumerate(files):
-                            print(f"{i + 1}. {display_name}")
-                        file_option = input(f"{PREFIX_OUT}\nEscolha um arquivo para baixar ou {ERROR}'b' {HEADER}para voltar:{WHITE} {RESET}")
-                        if file_option.isdigit():
-                            file_index = int(file_option) - 1
-                            _, file_url, file_type = files[file_index]
-                            if file_type == "File":
-                                file_name = f"./downloads/{files[file_index][0].split(' (')[0]}"
-                                download_file(file_url, file_name)
-                    else:
-                        print(f"\n{ERROR}Arquivos ainda sendo processados, tente novamente em breve.{RESET}\n")
-                elif action == '2':
-                    clear_screen()
-                    repo_url = repositories[repo_index]['url']
-                    clone_repository(repo_url, repo_name)
-            elif repo_option.lower() == 'b':
-                continue
+                    print(f"\n{WHITE}------------------------{RESET}\n")
+                    print(f"\nOpções para {repo_name}:\n\n{WHITE}1.{HEADER} Ver arquivos\n\n{WHITE}2.{HEADER} Clonar repositório{RESET}")
+                    print(f"\n{WHITE}------------------------{RESET}\n")
+                    action = input(f"{PREFIX_OUT} {WHITE}Escolha uma ação: {WHITE}{RESET} ")
+                    if action == '1':
+                        clear_screen()
+                        if repo_name in files_dict:
+                            files = files_dict[repo_name]
+                            for i, (display_name, file_url, file_type) in enumerate(files):
+                                print(f"{i + 1}. {display_name}")
+                            file_option = input(f"{PREFIX_OUT}\nEscolha um arquivo para baixar ou 'b' para voltar:{WHITE} {RESET}")
+                            if file_option.isdigit():
+                                file_index = int(file_option) - 1
+                                _, file_url, file_type = files[file_index]
+                                if file_type == "File":
+                                    file_name = f"./downloads/{files[file_index][0].split(' (')[0]}"
+                                    download_file(file_url, file_name)
+                        else:
+                            print(f"\n{ERROR}Arquivos ainda sendo processados, tente novamente em breve.{RESET}\n")
+                    elif action == '2':
+                        clear_screen()
+                        repo_url = repositories[repo_index]['url']
+                        clone_repository(repo_url, repo_name)
+                elif repo_option.lower() == 'p' and current_page < total_pages - 1:
+                    current_page += 1
+                elif repo_option.lower() == 'n' and current_page > 0:
+                    current_page -= 1
+                elif repo_option.lower() == 'b':
+                    break
         elif option == '2':
             clear_screen()
             print(f"\n{ERROR}Programa encerrado.{RESET}\n")
